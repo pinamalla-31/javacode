6e750b02ce4c74021f88857074470c58e1b7348a
pinamalla-31 <deepikapinamalla31@gmail.com>
2022-09-09 19:33:18 +0530

    depika

---
+++ src/org/java/programs/ArrayTest.java
@@ -0,0 +1,21 @@
+package org.java.programs;
+
+import java.util.Scanner;
+
+public class ArrayTest {
+	public static void main(String[] args) {
+		int a[] = new int [5];
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the values");
+		for(int i=1;i<5;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		System.out.println("output:");
+		for(int i=1;i<5;i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+
+}
